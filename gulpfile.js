4fe9e5e024d50bf1f159b0ad2e83df5305c2df95
mavl <mvaronamac@gmail.coml>
2019-08-10 11:29:26 +0200

    finish layout

---
+++ gulpfile.js
@@ -0,0 +1,25 @@
+const gulp = require('gulp');
+const sass = require('gulp-sass');
+const autoprefixer = require('gulp-autoprefixer');
+
+function css() {
+    return gulp
+        .src('scss/app.scss')
+        .pipe(autoprefixer({
+            overrideBrowserslist: ['last 2 versions'],
+            cascade: false
+        }))
+        .pipe(sass({
+            outputStyle: 'expanded'
+        }))
+        .pipe(gulp.dest('css'));
+}
+
+function watchFiles() {
+    gulp.watch('scss/*.scss', css);
+    gulp.watch('index.html');
+}
+
+// Register function as task
+gulp.task('css', css);
+gulp.task('watch', gulp.parallel(watchFiles));
